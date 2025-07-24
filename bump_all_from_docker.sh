38bc46ca013f21d4b3ed5a92c197dd6a5260fd07
Fawang <fwcheung@outlook.com>
2025-07-24 10:40:57 +0800

    chore: remove useless file

--- bump_all_from_docker.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker build -t xs-ready-to-run-env . && \
-docker run -it --rm --volume=$(pwd):/root/workbench --workdir /root/workbench xs-ready-to-run-env bash -l auto_bump.sh
